c9f5cf4fd96fc5f2942a5701d206c507bae7d57c
Mikołaj Kaczmarek <mikolaj.s.kaczmarek@outlook.com>
2023-09-06 17:14:43 +0200

    Add time sleep before creating bastion session

--- infra/modules/database/bastion.tf
+++ infra/modules/database/bastion.tf
@@ -21,7 +21,7 @@ resource "oci_bastion_session" "db_bastionsession" {
   target_resource_details {
 
     session_type       = "MANAGED_SSH"
-    target_resource_id = oci_core_instance.db_instance.id
+    target_resource_id = time_sleep.db_instance_id.triggers["db_instance_id"]
 
     target_resource_operating_system_user_name = "ubuntu"
     target_resource_port                       = "22"
@@ -30,4 +30,13 @@ resource "oci_bastion_session" "db_bastionsession" {
   session_ttl_in_seconds = 10800
 
   display_name = "bastionsession-db-instance"
+}
+
+
+resource "time_sleep" "db_instance_id" {
+  create_duration = "90s"
+
+  triggers = {
+    db_instance_id = oci_core_instance.db_instance.id
+  }
 }
