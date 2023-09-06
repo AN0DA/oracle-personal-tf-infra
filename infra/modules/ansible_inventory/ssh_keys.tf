f62dc43a193ea6714e07795331f9371b7bb7eaf0
Mikołaj Kaczmarek <mikolaj.s.kaczmarek@outlook.com>
2023-09-06 17:13:59 +0200

    Fix invalid file content reference

--- infra/modules/ansible_inventory/ssh_keys.tf
+++ infra/modules/ansible_inventory/ssh_keys.tf
@@ -11,7 +11,7 @@ resource "local_file" "db_instance_private_key" {
 }
 
 resource "local_file" "db_bastion_private_key" {
-  content         = var.base_instance_private_key
+  content         = var.db_bastion_private_key
   filename        = "${var.playbooks_path}/ssh_keys/firefly_db_bastion.pem"
   file_permission = "400"
 }
