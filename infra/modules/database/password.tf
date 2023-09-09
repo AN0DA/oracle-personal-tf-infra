c0eafb59efcf86cbaa8c3918f834b89641464e84
Mikołaj Kaczmarek <mikolaj.s.kaczmarek@outlook.com>
2023-09-09 20:26:10 +0200

    Improve password rules

--- infra/modules/database/password.tf
+++ infra/modules/database/password.tf
@@ -1,11 +1,23 @@
 resource "random_password" "db_user_password" {
   length           = 24
+
   special          = true
   override_special = "\"'"
+
+    min_lower = 1
+  min_upper = 1
+  min_numeric = 1
+  min_special = 1
 }
 
 resource "random_password" "db_root_password" {
   length           = 24
-  special          = true
+
+    special          = true
   override_special = "\"'"
+
+  min_lower = 1
+  min_upper = 1
+  min_numeric = 1
+  min_special = 1
 }
